4c0e5bc6bdb73c9552b1c39cc8b89a4719d86546
Sabri Rapa <sabrina.rapa@hotmail.com>
2023-10-01 16:19:13 -0300

    feat: Personalized character model

---
+++ server/src/models/PersonalizedCharacter.model.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class PersonalizedCharacter extends Model {}
+  
+
+  User.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: "PersonalizedCharacter",
+      paranoid: true,
+    }
+  );
+  return PersonalizedCharacter;
+};
